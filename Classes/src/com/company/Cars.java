2d005308747a51602f50f02ff9dfc8983fb3f9dd
Murat Bilgin <bilginmurat07@gmail.com>
2022-02-20 16:53:48 +0300

    Classes comitted

---
+++ Classes/src/com/company/Cars.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Cars {
+
+        public String model;
+        public String color;
+        public int engine;
+        public int door;
+
+    }
+
---
+++ Classes/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Cars car1 = new Cars();
+    car1.color = "blue";
+    car1.model = "opel";
+    car1.engine = 2;
+    car1.door = 4;
+    System.out.println(car1.color);
+        System.out.println(car1.model);
+        System.out.println(car1.engine);
+        System.out.println(car1.door);
+    }
+}
